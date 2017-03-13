e3fb453f24eb265e0533360a2e6b86e06b8d5831
Quiterie Cazet <q.cazet@gmail.com>
2017-03-13 16:58:36 +0100

    Sixième exercice effectué

--- ex06.js
+++ ex06.js
@@ -51,3 +51,22 @@ getHumanTypeByAge(33);
 getHumanTypeByAge(140);
 
 //  écrire votre code sous ce commentaire
+
+
+function getHumanTypeByAge(nb) {
+    if (nb < 1 || nb >= 124) {
+        return "impossible... ou encore jamais vu";
+    } else if (nb < 12) {
+        return "enfant";
+    } else if (nb >= 12 && nb < 18) {
+        return "ado";
+    } else if (nb >= 18 && nb < 124){
+        return "adulte";
+    } 
+}
+
+console.log(getHumanTypeByAge(-5));
+console.log(getHumanTypeByAge(6));
+console.log(getHumanTypeByAge(17));
+console.log(getHumanTypeByAge(33));
+console.log(getHumanTypeByAge(140));
